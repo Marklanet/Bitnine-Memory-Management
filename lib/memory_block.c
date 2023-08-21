34b6751530ed656c54fd3f9b6b6ed19d935a59e9
Marklanet <77835523+Marklanet@users.noreply.github.com>
2023-08-21 07:31:38 +0300

    second commit

---
+++ lib/memory_block.c
@@ -0,0 +1,110 @@
+/* malloc_block.c: Block Structure */
+
+#include "malloc/block.h"
+#include "malloc/counters.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Functions */
+
+/**
+ * Allocate a new block on the heap using sbrk:
+ *
+ *  1. Determined aligned amount of memory to allocate.
+ *  2. Allocate memory on the heap.
+ *  3. Set allocage block properties.
+ *
+ * @param   size    Number of bytes to allocate.
+ * @return  Pointer to data portion of newly allocate block.
+ **/
+Block *block_allocate(size_t size)
+{
+    intptr_t capacity = ALIGN(size + sizeof(Block));
+    Block *block = sbrk(capacity);
+    if (block == SBRK_FAILURE)
+    {
+        return NULL;
+    }
+
+    block->size = size;
+    block->prev = block;
+    block->next = block;
+
+    Counters[HEAP_SIZE] += capacity;
+    Counters[BLOCKS]++;
+    Counters[GROWS]++;
+    return block;
+}
+
+/**
+ * Attempt to release memory used by block to heap:
+ *
+ *  1. If the block is at the end of the heap.
+ *  2. The block size meets the trim threshold.
+ *
+ * @param   block   Pointer to block to release.
+ * @return  Whether or not the release completed successfully.
+ **/
+bool block_release(Block *block)
+{
+    // TODO: Implement block release
+    // size_t allocated = 0;
+    // Counters[BLOCKS]--;
+    // Counters[SHRINKS]++;
+    // Counters[HEAP_SIZE] -= allocated;
+    return false;
+}
+
+/**
+ * Detach specified block from its neighbors.
+ *
+ * @param   block   Pointer to block to detach.
+ * @return  Pointer to detached block.
+ **/
+Block *block_detach(Block *block)
+{
+    // TODO: Detach block from neighbors by updating previous and next block
+    return block;
+}
+
+/**
+ * Attempt to merge source block into destination.
+ *
+ *  1. Compute end of destination and start of source.
+ *
+ *  2. If they both match, then merge source into destionation by giving the
+ *  destination all of the memory allocated to source.
+ *
+ * @param   dst     Destination block we are merging into.
+ * @param   src     Source block we are merging from.
+ * @return  Whether or not the merge completed successfully.
+ **/
+bool block_merge(Block *dst, Block *src)
+{
+    // TODO: Implement block merge
+    // Counters[MERGES]++;
+    // Counters[BLOCKS]--;
+    return false;
+}
+
+/**
+ * Attempt to split block with the specified size:
+ *
+ *  1. Check if aligned block size is sufficient for requested size and header
+ *  Block.
+ *  2. Split specified block into two blocks.
+ *
+ * @param   block   Pointer to block to split into two separate blocks.
+ * @param   size    Desired size of the first block after split.
+ * @return  Pointer to original block if the split was successfully or NULL if a
+ * split was not possible.
+ **/
+Block *block_split(Block *block, size_t size)
+{
+    // TODO: Implement block split
+    // Counters[SPLITS]++;
+    // Counters[BLOCKS]++;
+    return NULL;
+}
---
+++ test.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+#define MEMORY_SIZE 1024 // Total size of memory in bytes
+
+// Structure to represent a memory block
+typedef struct Block
+{
+    size_t size;
+    struct Block *next;
+    int is_free;
+} Block;
+
+// The starting address of the memory pool
+void *memory_pool;
+
+// Function to initialize the memory pool
+void initialize_memory_pool()
+{
+    memory_pool = malloc(MEMORY_SIZE);
+    if (!memory_pool)
+    {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    Block *initial_block = (Block *)memory_pool;
+    initial_block->size = MEMORY_SIZE - sizeof(Block);
+    initial_block->next = NULL;
+    initial_block->is_free = 1;
+}
+
+// Function to allocate memory
+void *my_malloc(size_t size)
+{
+    Block *curr_block = (Block *)memory_pool;
+    while (curr_block)
+    {
+        if (curr_block->is_free && curr_block->size >= size)
+        {
+            if (curr_block->size > size + sizeof(Block))
+            {
+                Block *new_block = (Block *)((char *)curr_block + sizeof(Block) + size);
+                new_block->size = curr_block->size - size - sizeof(Block);
+                new_block->next = curr_block->next;
+                new_block->is_free = 1;
+                curr_block->size = size;
+                curr_block->next = new_block;
+            }
+            curr_block->is_free = 0;
+            return (char *)curr_block + sizeof(Block);
+        }
+        curr_block = curr_block->next;
+    }
+    return NULL; // Out of memory
+}
+
+// Function to free memory
+void my_free(void *ptr)
+{
+    if (!ptr)
+    {
+        return;
+    }
+    Block *curr_block = (Block *)((char *)ptr - sizeof(Block));
+    curr_block->is_free = 1;
+}
+
+// Function to display memory status
+void display_memory()
+{
+    Block *curr_block = (Block *)memory_pool;
+    printf("Memory Status:\n");
+    while (curr_block)
+    {
+        printf("Block: %p, Size: %lu, Free: %d\n", curr_block, curr_block->size, curr_block->is_free);
+        curr_block = curr_block->next;
+    }
+}
+*/
+int main()
+{
+    // initialize_memory_pool();
+
+    void *ptr1 = my_malloc(100);
+    void *ptr2 = my_malloc(200);
+    void *ptr3 = my_malloc(300);
+
+    display_memory();
+
+    my_free(ptr2);
+
+    display_memory();
+
+    my_free(ptr1);
+    my_free(ptr3);
+
+    display_memory();
+
+    free(memory_pool);
+
+    return 0;
+}
